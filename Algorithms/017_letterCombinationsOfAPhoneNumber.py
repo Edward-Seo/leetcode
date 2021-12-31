be9ea6d2c867a418223d3b1a80b6ef017acf4bb4
Edward-Seo <86662131+Edward-Seo@users.noreply.github.com>
2021-12-31 12:06:26 +1100

    Update 017_letterCombinationsOfAPhoneNumber.py

--- Algorithms/017_letterCombinationsOfAPhoneNumber.py
+++ Algorithms/017_letterCombinationsOfAPhoneNumber.py
@@ -1,20 +1,21 @@
-Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+# Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
 
-A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+# A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
 
-Example 1:
+# Example 1:
 
-Input: digits = "23"
-Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
-Example 2:
+# Input: digits = "23"
+# Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+# Example 2:
 
-Input: digits = ""
-Output: []
-Example 3:
+# Input: digits = ""
+# Output: []
+# Example 3:
 
-Input: digits = "2"
-Output: ["a","b","c"]
+# Input: digits = "2"
+# Output: ["a","b","c"]
   
+# Solution
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         digit_list = []
